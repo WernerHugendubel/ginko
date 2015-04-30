6497f68c2ad0a715c8a1b531c0a5d759f8906207
WernerHugendubel <freiwe@gmail.com>
2015-04-30 18:39:49 +0200

    added comments to business logic added hibernate libraries

--- ginko/src/it/unibz/internet/business/MealReservationService.java
+++ ginko/src/it/unibz/internet/business/MealReservationService.java
@@ -9,21 +9,38 @@ import it.unibz.internet.domain.Restriction;
 
 import java.util.List;
 
+/**
+ * Business logic for Meal Reservation
+ * @author Werner Frei <freiwe@gmail.com>
+ *
+ */
 public class MealReservationService {
 
 	Dao dao;
 
+	/**
+	 * Constructor
+	 */
 	public MealReservationService() {
 		// TODO: retrieve DAO implementation through factory class
 		dao = new DaoImpl();
-		
 	}
 
+	
+	/**
+	 * Returns a list of patients
+	 * @return list of patients
+	 */
 	public List<Patient> getPatients() {
 		return dao.getPatients();
 	}
 
-	public boolean addPatient(String patname, int patientbednr) {
+	/**
+	 * Add a new patient 
+	 * @param patname name of the patient
+	 * @param patientbednr bednr the patient is in 
+	 */
+	public void addPatient(String patname, int patientbednr) {
 		Patient pat = new Patient();
 		pat.setName(patname);
 		pat.setBednr(patientbednr);
@@ -31,13 +48,23 @@ public class MealReservationService {
 		// TODO: Business logic: check patient constraints before save
 		// save to db
 		this.dao.addNew(pat);
-		return true;
 	}
 
-	public Patient getPatient(int id) {
-		return this.dao.getPatient(id);
+	/**
+	 * Get a patient by their id
+	 * @param patientId the id of the patient
+	 * @return the patient
+	 */
+	public Patient getPatient(int patientId) {
+		return this.dao.getPatient(patientId);
 	}
 
+	/**
+	 * Update a patient with given parameters
+	 * @param patientid id of the patient
+	 * @param patname name of the patient
+	 * @param patientbednr bednr the patient is in 
+	 */
 	public void updatePatient(int patientid, String patname, int patientbednr) {
 		Patient pat = this.dao.getPatient(patientid);
 		pat.setName(patname);
@@ -48,22 +75,47 @@ public class MealReservationService {
 		this.dao.updatePatient(pat);
 	}
 
+	/**
+	 * Get a list of orders for a single patient 
+	 * @param patientId the id of the patient
+	 * @return list of orders
+	 */
 	public List<Order> getOrderList(int patientId) {
 		return this.dao.getOrderList(patientId);
 	}
 
+	/**
+	 * Retrieve an order by id
+	 * @param orderId the id of the order to retrieve
+	 * @return the order with id orderId
+	 */
 	public Order getOrder(int orderId) {
 		return this.dao.getOrder(orderId);
 	}
 
+	/**
+	 * List all dishes available
+	 * @return a list of all available dishes
+	 */
 	public List<Dish> getDishs() {
 		return this.dao.getDishs();
 	}
 
+	/**
+	 * Find a dish by id
+	 * @param dishId the id of the dish to search
+	 * @return the dish 
+	 */
 	public Dish getDish(int dishId) {
 		return this.dao.getDish(dishId);
 	}
 
+	/**
+	 * Add a dish to an order 
+	 * @param orderId id of the order where we insert dish
+	 * @param dishId the id of the dish to insert
+	 * @throws Exception if dish is already present in order
+	 */
 	public void addDishToOrder(int orderId, int dishId) throws Exception {
 		Dish d = this.getDish(dishId);
 		Order o = this.getOrder(orderId);
@@ -85,6 +137,11 @@ public class MealReservationService {
 		this.dao.updateOrderDetails(o);
 	}
 
+	/**
+	 * Removes a dish from an order
+	 * @param orderId id of the order
+	 * @param dishId id of the dish to remove
+	 */
 	public void removeDishFromOrder(int orderId, int dishId) {
 		Dish d = this.getDish(dishId);
 		Order o = this.getOrder(orderId);
